4e22111ec3408c4fc072c32bf70fb7266b7cdc35
ZhuChongLin <8210856@qq.com>
2024-03-31 16:20:46 +0800

    日志静态调用门面类

---
+++ src/Log.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Log\Facade;
+
+use Stringable;
+use ViSwoole\Log\Contract\LogDriveInterface;
+use ViSwoole\Log\LogLevel;
+use ViSwoole\Log\LogManager;
+
+/**
+ * 日志管理器快捷调用门面类
+ */
+class Log
+{
+  /**
+   * 日志门面类
+   *
+   * @method static void emergency(string $level, string|Stringable $message, array $context = []) 紧急情况。
+   * @method static void log(string $level, string|Stringable $message, array $context = []) 记录具有任意级别的日志。
+   * @method static void alert(string|Stringable $message, array $context = []) 必须立即采取行动。
+   * @method static void critical(string|Stringable $message, array $context = []) 严重情况。
+   * @method static void error(string|Stringable $message, array $context = []) 不需要立即采取行动的运行时错误，但通常应记录和监视。
+   * @method static void warning(string|Stringable $message, array $context = []) 不是错误的异常情况。
+   * @method static void notice(string|Stringable $message, array $context = []) 正常但重要的事件。
+   * @method static void info(string|Stringable $message, array $context = []) 有趣的事件。
+   * @method static void debug(string|Stringable $message, array $context = []) 详细的调试信息。
+   * @method static void sql(string|Stringable $message, array $context = []) SQL日志。
+   * @method static void write(Stringable|string $message, array $context = [], string $level = LogLevel::INFO) 直接写入日志
+   * @method static bool save(array $logRecords) 保存日志（无需手动调用, 协程结束会自动调用）
+   * @method static bool clearRecord() 清除缓存日志
+   * @method static array getRecord() 获取缓存日志
+   * @method static LogDriveInterface channel(string $name) 设置日志通道
+   * @method static bool hasChannel(string $name) 判断通道是否存在
+   * @method static void setDefaultChannel(string $name) 设置默认日志通道
+   * @method static void addChannel(string $name, LogDriveInterface $drive) 添加日志通道
+   * @method static LogDriveInterface[]|null getChannels(?string $name = null) 获取日志通道
+   */
+  public static function __callStatic($method, $params)
+  {
+    return call_user_func_array([static::__make(), $method], $params);
+  }
+
+  /**
+   * 创建日志管理器但实例
+   *
+   * @param string|null $configPath 配置文件路径
+   * @return LogManager
+   */
+  public static function __make(string $configPath = null): LogManager
+  {
+    static $instance = null;
+    if ($instance === null) $instance = new LogManager($configPath);
+    return $instance;
+  }
+}
